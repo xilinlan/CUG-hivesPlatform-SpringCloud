1bab1452f9af32677a4fb850823ddb66b4fd5837
zhangwentao <2298805496@qq.com>
2023-03-31 21:16:26 +0800

    搭建好了微服务exchange,user项目，Nacos注册配置中心，以及网关转发服务

---
+++ hives-gateway/src/test/java/com/hives/gateway/HivesGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hives.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HivesGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
