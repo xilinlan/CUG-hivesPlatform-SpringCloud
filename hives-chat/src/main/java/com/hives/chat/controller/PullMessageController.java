310788dcb774dff609a1b8dcc2a9ca06e4e6d0d2
zhangwentao <2298805496@qq.com>
2023-04-25 15:02:31 +0800

    mongoTemplate引入

--- hives-chat/src/main/java/com/hives/chat/controller/PullMessageController.java
+++ hives-chat/src/main/java/com/hives/chat/controller/PullMessageController.java
@@ -1,9 +1,14 @@
 package com.hives.chat.controller;
 
+import com.hives.chat.entity.MessageEntity;
+import com.hives.common.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
@@ -23,6 +28,9 @@ public class PullMessageController {
 
     private RedisTemplate redis;
 
+    @Resource
+    private MongoTemplate mongoTemplate;
+
     @Autowired
     public PullMessageController(RedisTemplate redis) {
         this.redis = redis;
@@ -37,6 +45,22 @@ public class PullMessageController {
                 .collect(Collectors.joining("-"));
         System.out.println(key);
         return redis.opsForList().range(key, 0, -1);
+    }
+
+    @GetMapping("/getMsg")
+    public R GetMessage(){
+//        MessageEntity messageEntity=new MessageEntity();
+//        messageEntity.setFromId(3L);
+//        messageEntity.setToId(4L);
+//        messageEntity.setContent("hello");
+//        messageEntity.setType("1");
+//        messageEntity.setTime(new Date());
+
+        // mongoTemplate.save(messageEntity);
+
+        List<MessageEntity> all = mongoTemplate.findAll(MessageEntity.class);
+        // System.out.println(all);
+        return R.ok().put("data",all);
     }
 
 }
