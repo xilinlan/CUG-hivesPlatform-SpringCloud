9588e1b19985a34c24b4bfa25c1374b399458171
zhangwentao <2298805496@qq.com>
2023-05-06 17:03:40 +0800

    --task=1019893 --user=张韬 测试类

---
+++ hives-exchange/src/test/java/com/hives/exchange/controller/ReplyControllerTest.java
@@ -0,0 +1,39 @@
+package com.hives.exchange.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: zhangtao
+ * @Date: 2023/05/06/15:45
+ * @Description:
+ */
+class ReplyControllerTest {
+
+    @Test
+    void firstLevelComments() {
+    }
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void info() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ hives-user/src/test/java/com/hives/user/controller/AdminControllerTest.java
@@ -0,0 +1,35 @@
+package com.hives.user.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: zhangtao
+ * @Date: 2023/05/06/17:01
+ * @Description:
+ */
+class AdminControllerTest {
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void info() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
